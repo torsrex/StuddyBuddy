869cb1892ac9836c687fa1c16eb454a1d59e70a3
Torstein ‪ <Torstein >
2017-02-21 17:48:10 +0100

    Fixed models.py

--- forum/models.py
+++ forum/models.py
@@ -3,6 +3,12 @@ from django.db import models
 
 # Create your models here.
 
+class Topic(models.Model):
+    topic_name = models.CharField(max_length=60)
+    topic_desc = models.CharField(max_length=200)
+    topic_created = models.DateTimeField('Date created', auto_now_add=True)  # Automatically stores creation save
+    topic_updated = models.DateTimeField('Date updated', auto_now_add=True)  # Automatically updates on save
+
 
 class Question(models.Model):
     question_name = models.CharField(max_length=None)
@@ -19,10 +25,3 @@ class Answer(models.Model):
     answer_text = models.TextField()
     answer_created = models.DateTimeField('Date created', auto_now_add=True)  # Automatically stores creation save
     answer_updated = models.DateTimeField('Date updated', auto_now_add=True)  # Automatically updates on save
-
-
-class Topic(models.Model):
-    topic_name = models.CharField(max_length=60)
-    topic_desc = models.CharField(max_length=200)
-    topic_created = models.DateTimeField('Date created', auto_now_add=True)  # Automatically stores creation save
-    topic_updated = models.DateTimeField('Date updated', auto_now_add=True)  # Automatically updates on save
