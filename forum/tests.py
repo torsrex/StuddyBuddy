eb909448de35df9257a4f1f35b1f7c3d27374152
Herleik Holtan <herleik.holtan@gmail.com>
2017-03-22 09:54:28 +0100

    Added coverage

--- forum/tests.py
+++ forum/tests.py
@@ -1,5 +1,6 @@
 from django.test import Client, tag, LiveServerTestCase,TestCase
 from forum.models import Topic, User
+import time
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
@@ -27,11 +28,6 @@ class siteTestCase(TestCase):
         response = client.get('/forum/')  # expecting 200
         self.assertEquals(response.status_code, 200)
 
-    def test_forum(self):
-        client = Client()
-        response = client.get('/forum/')  # expecting 200
-        self.assertEquals(response.status_code, 200)
-
     def test_topics_appear_on_index_page(self):
         client = Client()
         response = client.get('/forum/')  # expecting 200
@@ -41,7 +37,7 @@ class siteTestCase(TestCase):
 
     def test_topic_page(self):
         c=Client()
-        resp=c.get('/forum/topics/1/') #expecting 200
+        resp=c.get('/forum/topics/1/') #expecting 200, as a topic with id=1 is created.
         self.assertEquals(resp.status_code, 200)
 
 
@@ -60,16 +56,6 @@ class permissionTestCase(TestCase):
             topic_desc="veldig smud, veldig test.",
             id=1
         )
-
-    @tag('notworking')
-    def test_need_to_be_logged_in_to_post_questions(self):
-        c = Client()
-        resp = c.get('/forum/topics/1/new_question')
-        print('resp1= '+str(resp))
-        c.force_login(self.student)
-        resp = c.get('/forum/topics/1/new_question')
-        print('resp2= '+str(resp))
-        self.assertEquals(resp.status_code, 301)
 
     #@tag('notworking')
     #def teachers_can_delete_questions(self):
@@ -92,11 +78,8 @@ class permissionTestCase(TestCase):
 
 
 @tag('selenium')
-class MySeleniumTests(LiveServerTestCase):
-
+class MySeleniumTests(LiveServerTestCase): #HUSK AT DISSE BRUKER LIVE-DATABASEN
     def setUp(self):
-        self.teacher = User.objects.create_user(
-            username='Pekka', email='pekka@ntnu.no', password='abraham', id=0)
         self.selenium = webdriver.Firefox()
         super(MySeleniumTests, self).setUp()
 
@@ -104,23 +87,101 @@ class MySeleniumTests(LiveServerTestCase):
         self.selenium.quit()
         super(MySeleniumTests, self).tearDown()
 
-    def test_login(self):
-        selenium = self.selenium
-        #Opening the link we want to test
+     #Finner ikke ut om brukeren eksisterer i den ekte databasen, bare testbasen.
+    def test_registration(self):
+        selenium=self.selenium
+        selenium.get('http://127.0.0.1:8000/forum/register/')
+
+        time.sleep(0.5)
+
+        username = selenium.find_element_by_name('username')
+        password = selenium.find_element_by_name('password')
+        email=selenium.find_element_by_name('email')
+
+        register=selenium.find_element_by_id('register_button')
+
+        name='random'+str(round(time.time(),0))
+
+        time.sleep(0.5)
+        username.send_keys(name)
+        password.send_keys('stuff')
+        email.send_keys('dfssdf@ntnu.no')
+        time.sleep(0.5)
+        register.send_keys(Keys.RETURN)
+        time.sleep(0.8)
+        """
+        users=User.objects.all() #Dette returnerer test-databasebrukerne
+        userFound=False
+        for u in users:
+            print('\n',u.username)
+            if name == u.username:
+                userFound=True"""
+        assert 'Topics' in selenium.title
+
+    def login_as_student(self,selenium): #Requires a user with username student and password qwerty
         selenium.get('http://127.0.0.1:8000/forum/login')
-        #find the form element
+        # find the form element
         username = selenium.find_element_by_name('username')
         password = selenium.find_element_by_name('password')
 
         login = selenium.find_element_by_id('login_button')
 
-        #Fill the form with data
-        username.send_keys('Pekka')
-        password.send_keys('abraham')
-        #submitting the form
-        login.click()
-            #send_keys(Keys.RETURN)
+        # Fill the form with data
+        username.send_keys('student')
+        password.send_keys('qwerty')
+        # submitting the form
+        login.send_keys(Keys.RETURN)
 
-        #check the returned result
-        print(selenium.title)
+    def login_as_admin(self,selenium):
+        selenium.get('http://127.0.0.1:8000/forum/login')
+        # find the form element
+        username = selenium.find_element_by_name('username')
+        password = selenium.find_element_by_name('password')
+
+        login = selenium.find_element_by_id('login_button')
+
+        # Fill the form with data
+        username.send_keys('admin')
+        password.send_keys('admin')
+        # submitting the form
+        login.send_keys(Keys.RETURN)
+
+    @tag('slow')
+    def test_login(self):
+        selenium = self.selenium
+        self.login_as_admin(selenium)
+        time.sleep(0.8) #If this test fails, try a longer sleep time.
         assert 'Topics' in selenium.title
+
+    @tag('slow')
+    def test_must_be_logged_in_to_post_questions(self):
+        selenium = self.selenium
+        selenium.get('http://127.0.0.1:8000/forum/topics/1/')
+        selenium.find_element_by_id('registration_button').click()
+        time.sleep(0.3)
+        assert selenium.current_url=='http://127.0.0.1:8000/forum/login/?next=/forum/topics/1/new_question/'
+        self.login_as_student(selenium)
+        time.sleep(0.3)
+        selenium.get('http://127.0.0.1:8000/forum/topics/1/')
+        selenium.find_element_by_id('registration_button').click()
+        """selenium.find_element_by_id('question_name').send_keys('test')
+        selenium.find_element_by_id('question_text').send_keys('testtext')
+        selenium.find_element_by_id('button').send_keys(Keys.RETURN)"""
+        time.sleep(0.3)
+        assert selenium.current_url=='http://127.0.0.1:8000/forum/topics/1/new_question/'
+
+    def test_teacher_can_remove_post(self):
+        selenium=self.selenium
+        self.login_as_admin(selenium)
+        time.sleep(0.6)
+        selenium.find_element_by_id('t1').click() #Krever at det finnes en topic
+        time.sleep(0.8)
+        selenium.find_element_by_id('b1').click() #Krever at det finnes et spørsmål
+        time.sleep(0.4)
+        selenium.find_element_by_id('remove').click()
+        time.sleep(0.4)
+        assert True #Hvis sletteknappen finnes, har alt gått bra.
+
+
+
+
