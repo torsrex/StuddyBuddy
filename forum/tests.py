a1d701f203a1b9861f029b54d61c1574735d7c69
Herleik Holtan <herleik.holtan@gmail.com>
2017-04-05 13:46:32 +0200

    removed useless function

--- forum/tests.py
+++ forum/tests.py
@@ -7,7 +7,7 @@ from forum.models import Topic, User
 
 class siteTestCase(TestCase):
     def setUp(self):
-        self.topic_1 = Topic.objects.create( #Creating a topic in the test database
+        self.topic_1 = Topic.objects.create(  # Creating a topic in the test database
             topic_name="smud test topic",
             topic_desc="veldig smud, veldig test.",
             id=1
@@ -33,11 +33,12 @@ class siteTestCase(TestCase):
 
 
 class permissionTestCase(TestCase):
-    def setUp(self): #Makes users and a topic, so it doesn't have to be done in every test.
+    def setUp(self):  # Makes users and a topic, so it doesn't have to be done in every test.
         self.teacher = User.objects.create_user(
             username='Pekka', email='pekka@ntnu.no', password='abraham', )
         permission = Permission.objects.get(codename='delete_question')
-        self.teacher.user_permissions.add(permission) #Permissions must be added manually, as we bypass the form when creating users this way
+        self.teacher.user_permissions.add(
+            permission)  # Permissions must be added manually, as we bypass the form when creating users this way
         self.student = User.objects.create_user(
             username='GenericStudent', email='generic@stud.ntnu.no', password='qwerty'
         )
@@ -49,43 +50,44 @@ class permissionTestCase(TestCase):
             topic_desc="veldig smud, veldig test.",
         )
 
-    def test_post_question(self): #Are we able to post questions and have them appear in the database?
+    def test_post_question(self):  # Are we able to post questions and have them appear in the database?
         c = Client()
         c.force_login(self.student)
-        c.post('/forum/topics/1/new_question/', #Post makes a POST-request to the link in the first argument, sending in the dictionary in the second argument in the request.
-                      {'question_name': "balle", 'question_text': "yoyoyo", 'question_topic': self.topic_1.id})
-        questions = Question.objects.all() #Fetches all the Question objects in the database
+        c.post('/forum/topics/1/new_question/',
+               # Post makes a POST-request to the link in the first argument, sending in the dictionary in the second argument in the request.
+               {'question_name': "balle", 'question_text': "yoyoyo", 'question_topic': self.topic_1.id})
+        questions = Question.objects.all()  # Fetches all the Question objects in the database
         questionFound = False
         for q in questions:
             if q.question_name == "balle":  # Does the question exist in the database?
                 questionFound = True
         assert questionFound
 
-    def test_teachers_add_topic(self): #Are we able to create topics and have them appear in the database?
+    def test_teachers_add_topic(self):  # Are we able to create topics and have them appear in the database?
         c = Client()
         c.force_login(self.teacher)
         resp = c.post('/forum/new_topic/', {'topic_name': 'ekstremt smud topic', 'topic_desc': 'uhyre smud desc'})
-        topics = Topic.objects.all() #Fetches all the Topic objects in the database
+        topics = Topic.objects.all()  # Fetches all the Topic objects in the database
         topicFound = False
         for t in topics:
             if t.topic_name == "ekstremt smud topic":
                 topicFound = True
         assert topicFound
 
-    def test_create_user(self): #Are we able to create users and have them appear in the database?
+    def test_create_user(self):  # Are we able to create users and have them appear in the database?
         c = Client()
         c.post('/forum/register/',
-                      {'username': 'testteacher', 'password': 'smud passord', 'email': 'smud@ntnu.no'})
+               {'username': 'testteacher', 'password': 'smud passord', 'email': 'smud@ntnu.no'})
         c.post('/forum/register/',
                {'username': 'teststudent', 'password': 'smud passord', 'email': 'smud@stud.ntnu.no'})
-        users = User.objects.all() #Fetches all the User objects in the database
+        users = User.objects.all()  # Fetches all the User objects in the database
         userFound = False
         for u in users:
             if u.username == "testteacher":
                 userFound = True
         assert userFound
 
-    def test_upvote_and_downvote(self): #Are we able to register and store upvotes and downvotes in the database?
+    def test_upvote_and_downvote(self):  # Are we able to register and store upvotes and downvotes in the database?
         self.question_1 = Question.objects.create(
             question_name="testquest",
             question_text="testdesc",
@@ -96,11 +98,11 @@ class permissionTestCase(TestCase):
         c.force_login(self.student)
         c.post('/forum/up_vote/',
                {'pk_question': self.question_1.id, 'topic': self.question_1.question_topic.id, 'user': self.student})
-        assert self.question_1.votes.count() == 1 #After the upvote, the number of votes registered on question_1 should be 1.
+        assert self.question_1.votes.count() == 1  # After the upvote, the number of votes registered on question_1 should be 1.
         c.post('/forum/down_vote/', {'pk_question': self.question_1.id, 'topic': self.question_1.question_topic.id})
-        assert self.question_1.votes.count() == 0 #After the downvote, the number of votes registered on question_1 should be 0 again.
+        assert self.question_1.votes.count() == 0  # After the downvote, the number of votes registered on question_1 should be 0 again.
 
-    def test_upvote_and_downvote_answer(self): #Does upvoting and downvoting answers work?
+    def test_upvote_and_downvote_answer(self):  # Does upvoting and downvoting answers work?
         self.question_1 = Question.objects.create(
             question_name="testquest",
             question_text="testdesc",
@@ -114,37 +116,19 @@ class permissionTestCase(TestCase):
                {'answer_text': 'have', 'question': self.question_1.id, 'pk_question': self.question_1.id,
                 'topic': self.question_1.question_topic.id, 'user': self.student})
         answerpk = -1
-        for a in Answer.objects.all(): #Iterates through all the answers in the database
+        for a in Answer.objects.all():  # Iterates through all the answers in the database
             if a.answer_text == 'have':
-                answerpk = a.id #Finds the pk of the answer we just created by a post-request.
+                answerpk = a.id  # Finds the pk of the answer we just created by a post-request.
         c.post('/forum/up_vote_answer/',
-               {'pk_answer': answerpk, 'topic': self.question_1.question_topic.id, 'user': self.student, 'pk_question': self.question_1.id})
+               {'pk_answer': answerpk, 'topic': self.question_1.question_topic.id, 'user': self.student,
+                'pk_question': self.question_1.id})
         assert Answer.objects.get(pk=answerpk).votes.count() == 1
         c.post('/forum/down_vote_answer/',
-               {'pk_answer': answerpk, 'topic': self.question_1.question_topic.id, 'user': self.student, 'pk_question': self.question_1.id})
+               {'pk_answer': answerpk, 'topic': self.question_1.question_topic.id, 'user': self.student,
+                'pk_question': self.question_1.id})
         assert Answer.objects.get(pk=answerpk).votes.count() == 0
 
-    """
-    def test_delete_question(self):
-        self.question_1 = Question.objects.create(
-            question_name="testquest",
-            question_text="testdesc",
-            question_topic=self.topic_1,
-            user=self.student2,
-        )
-        questions = Question.objects.all()
-        assert len(questions) == 1
-        print('\nquestions:',questions)
-        c = Client()
-        c.force_login(self.student)
-        resp=c.post('/forum/'+str(self.question_1.id))
-        print('\nresp:',resp)
-        questions = Question.objects.all()
-        print('\nurl:','/forum/'+str(self.question_1.id)+'/')
-        assert len(questions) == 0
-    """
-
-    def test_add_answer(self): #Are we able to create answers to questions and have them appear in the database?
+    def test_add_answer(self):  # Are we able to create answers to questions and have them appear in the database?
         self.question_1 = Question.objects.create(
             question_name="testquest",
             question_text="testdesc",
@@ -178,7 +162,8 @@ class permissionTestCase(TestCase):
         resp = c.get('/forum/my_question')
         self.assertTrue(len(resp.context['my_question_list']) == 0)  # For another user, the list should still be empty.
 
-    def test_teachers_can_delete_all_questions(self): #Teachers should be able to delete questions they didn't make themselves.
+    def test_teachers_can_delete_all_questions(
+            self):  # Teachers should be able to delete questions they didn't make themselves.
         self.question_1 = Question.objects.create(
             question_name="testquest",
             question_text="testdesc",
@@ -190,9 +175,10 @@ class permissionTestCase(TestCase):
         resp = c.post('/forum/delete_question_in_index/',
                       {'pk_question': self.question_1.id, 'topic': self.question_1.question_topic.id})
         questions = Question.objects.all()
-        assert len(questions) == 0 #If the deletion was successful, we should have no questions left.
+        assert len(questions) == 0  # If the deletion was successful, we should have no questions left.
 
-    def test_search(self): #Are we able to search for questions and only get questions similar to what we ask for in return?
+    def test_search(
+            self):  # Are we able to search for questions and only get questions similar to what we ask for in return?
         self.question_1 = Question.objects.create(
             question_name="transistor",
             question_text="transistors mayne, how do they work?",
@@ -208,19 +194,20 @@ class permissionTestCase(TestCase):
 
         c = Client()
         c.force_login(self.student)
-        resp=c.post('/forum/search/?q=transistors') #Makes a search for "transistors" , which searches through the question_texts of all questions for a similar word.
-        transistorFound=False
-        jacobianFound=False
-        for q in resp.context['result']: #Iterates through the questions in the list returned by the search function.
-            if q['question_name'] == 'jacobian': #The jacobian question should not be returned
-                jacobianFound=True
-            if q['question_name']=='transistor': #The transistor question should not be returned
-                transistorFound=True
+        resp = c.post(
+            '/forum/search/?q=transistors')  # Makes a search for "transistors" , which searches through the question_texts of all questions for a similar word.
+        transistorFound = False
+        jacobianFound = False
+        for q in resp.context['result']:  # Iterates through the questions in the list returned by the search function.
+            if q['question_name'] == 'jacobian':  # The jacobian question should not be returned
+                jacobianFound = True
+            if q['question_name'] == 'transistor':  # The transistor question should not be returned
+                transistorFound = True
 
         assert not jacobianFound
         assert transistorFound
 
-    def test_mark_as_solved(self): #Are we able to mark questions as solved, and then mark them as unsolved again?
+    def test_mark_as_solved(self):  # Are we able to mark questions as solved, and then mark them as unsolved again?
         self.question_1 = Question.objects.create(
             question_name="testquest",
             question_text="testdesc",
@@ -231,16 +218,14 @@ class permissionTestCase(TestCase):
         c.force_login(self.student)
 
         q = Question.objects.get(pk=self.question_1.id)
-        assert not q.question_solved #The question should initially not be solved.
+        assert not q.question_solved  # The question should initially not be solved.
         resp = c.post('/forum/mark_as_solved/',
-                      {'pk_question': self.question_1.id, 'topic': self.question_1.question_topic.id}) #After this, the question should be marked as solved.
-        q=Question.objects.get(pk=self.question_1.id)
+                      {'pk_question': self.question_1.id,
+                       'topic': self.question_1.question_topic.id})  # After this, the question should be marked as solved.
+        q = Question.objects.get(pk=self.question_1.id)
         assert q.question_solved
         resp = c.post('/forum/mark_as_solved/',
-                      {'pk_question': self.question_1.id, 'topic': self.question_1.question_topic.id})#After this, the question should be marked as unsolved.
+                      {'pk_question': self.question_1.id,
+                       'topic': self.question_1.question_topic.id})  # After this, the question should be marked as unsolved.
         q = Question.objects.get(pk=self.question_1.id)
         assert not q.question_solved
-
-
-
-
